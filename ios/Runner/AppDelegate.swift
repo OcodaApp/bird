e960c31c1a67a5c266d1f9835968e356320631a0
mohamed sayed <mosayed.ezt@gmail.com>
2025-01-30 12:19:49 +0200

    first commit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyATuEq0qRHi33CVV_q9_RwuQ6JRm7C0fWY")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
